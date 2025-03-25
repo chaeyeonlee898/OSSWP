3a6bf7398a699499df074e10685ec67e242bfd59
chaeyeonlee898 <chaeyeonlee898@gmail.com>
2025-03-25 21:53:30 +0900

    FIX: 반복 입력 되도록 코드 수정

--- calculate.py
+++ calculate.py
@@ -14,14 +14,28 @@ def divide(a, b):
 if __name__ == '__main__':
     
     ### 사용자 입력
-    print('\n첫번째 숫자를 입력하세요.')
-    input1 = int(input('입력: '))
+    while True:
+        print('\n첫번째 숫자를 입력하세요.')
+        user_input = input('입력: ')
+        try:
+            input1 = int(user_input)
+            break
+        except ValueError:
+            print('잘못된 입력입니다, 다시 첫번째 숫자를 입력하세요.')
+
 
     print('\n원하는 사칙연산 기호 중 하나를 선택하세요. (+, -, *, /)')
     act = input('기호: ')
 
-    print('\n두번째 숫자를 입력하세요.')
-    input2 = int(input('입력: '))
+    while True:
+        print('\n두번째 숫자를 입력하세요.')
+        user_input = input('입력: ')
+        try:
+            input2 = int(user_input)
+            break
+        except ValueError:
+            print('잘못된 입력입니다, 다시 두번째 숫자를 입력하세요.')
+
 
     ### 연산 수행
     if act == '+':
